a8287dd5482eecf6bffe95b1c83970c6c61d9e5f
Obez99 <giuliogx@live.it>
2021-12-03 15:05:13 +0100

    Initial commit+ creates products and users arrays

---
+++ data/products.php
@@ -0,0 +1,63 @@
+<?php
+$products = [
+  [
+    "name" => "Apple",
+    "brand" => "",
+    "category" => "Food",
+    "price" => 1
+  ],
+  [
+    "name" => "Pasta",
+    "brand" => "Barilla",
+    "category" => "Food",
+    "price" => 1
+  ],
+  [
+    "name" => "Macbook Air",
+    "brand" => "Apple",
+    "category" => "Electronics",
+    "price" => 1077
+  ],
+  [
+    "name" => "Chair",
+    "brand" => "",
+    "category" => "Furniture",
+    "price" => 9
+  ],
+  [
+    "name" => "Shaving Cream",
+    "brand" => "Proraso",
+    "category" => "Selfcare",
+    "price" => 3
+  ],
+  [
+    "name" => "Cheese",
+    "brand" => "Galbani",
+    "category" => "Food",
+    "price" => 3
+  ],
+  [
+    "name" => "Airpods Pro",
+    "brand" => "Apple",
+    "category" => "Electronics",
+    "price" => 199
+  ],
+  [
+    "name" => "Face mask",
+    "brand" => "",
+    "category" => "Selfcare",
+    "price" => 5
+  ],
+  [
+    "name" => "Bed",
+    "brand" => "",
+    "category" => "Furniture",
+    "price" => 299
+  ],
+  [
+    "name" => "Chips",
+    "brand" => "SanCarlo",
+    "category" => "Food",
+    "price" => 1
+  ],
+];
---
+++ data/users.php
@@ -0,0 +1,38 @@
+<?php
+$users = [
+  [
+    "nome" => "Giulio",
+    "cognome" => "Gulizia",
+    "email" => "giuliogulizia@gmail.com"
+  ],
+  [
+    "nome" => "Case",
+    "cognome" => "Wall",
+    "email" => "case_wall@icloud.com"
+  ],
+  [
+    "nome" => "Meredith",
+    "cognome" => "Chen",
+    "email" => "meredith8829@gmail.com"
+  ],
+  [
+    "nome" => "Johnny",
+    "cognome" => "Benjamin",
+    "email" => "BenJohnny@gmail.com"
+  ],
+  [
+    "nome" => "Beatrice",
+    "cognome" => "Crane",
+    "email" => "beatricecrane@icloud.com"
+  ],
+  [
+    "nome" => "Danna",
+    "cognome" => "Weber",
+    "email" => "danna92@hotmail.com"
+  ],
+  [
+    "nome" => "Bernard",
+    "cognome" => "Walsh",
+    "email" => "bernardwalsh@gmail.com"
+  ],
+];
