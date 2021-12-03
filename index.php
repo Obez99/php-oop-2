c2d965eabfb5674eb0aa562bc082dbf316d087b5
Obez99 <giuliogx@live.it>
2021-12-03 15:31:15 +0100

    Creates Class for each user in data/user.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require_once "data/users.php";
 
 require "classes/User.php";
 
-$users[0] = new User($users[0]);
-echo $users[0]->getName();
-var_dump($users[0]);
+foreach ($users as $user) {
+  $user = new User($user);
+  var_dump($user);
+}
